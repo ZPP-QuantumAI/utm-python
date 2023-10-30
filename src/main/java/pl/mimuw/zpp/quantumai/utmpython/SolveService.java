7d2f250c715b29f98af0fb63485b2fc90a2477fb
JKuziola <jakub.kuziola1@gmail.com>
2023-10-30 14:12:50 +0100

    ZPP-10 first version of SolveFilter.java

--- src/main/java/pl/mimuw/zpp/quantumai/utmpython/SolveService.java
+++ src/main/java/pl/mimuw/zpp/quantumai/utmpython/SolveService.java
@@ -7,36 +7,28 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.Semaphore;
 
 import static java.io.File.createTempFile;
 
 @Service
 @Slf4j
 public class SolveService {
-    private static final Semaphore mutex = new Semaphore(1);
 
     public String solve(
             File pythonFile,
             File inputFile
     ) throws IOException, InterruptedException {
-        if (mutex.tryAcquire()) {
-            String pythonFileAbsolutePath = pythonFile.getAbsolutePath();
-            File outputFile = createTempFile("output", "txt");
-            ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonFileAbsolutePath)
-                    .redirectInput(inputFile)
-                    .redirectOutput(outputFile);
-            Process process = processBuilder.start();
-            process.waitFor();
-            String result = Files.readString(Path.of(outputFile.getAbsolutePath()));
-            deleteFile(outputFile);
-            mutex.release();
+        String pythonFileAbsolutePath = pythonFile.getAbsolutePath();
+        File outputFile = createTempFile("output", "txt");
+        ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonFileAbsolutePath)
+                .redirectInput(inputFile)
+                .redirectOutput(outputFile);
+        Process process = processBuilder.start();
+        process.waitFor();
+        String result = Files.readString(Path.of(outputFile.getAbsolutePath()));
+        deleteFile(outputFile);
 
-            return result;
-        }
-        else {
-            return null;
-        }
+        return result;
     }
 
     private static void deleteFile(File file) {
