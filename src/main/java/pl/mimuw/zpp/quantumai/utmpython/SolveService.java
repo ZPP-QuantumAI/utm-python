cfd50c859e806c028cff13db937cfeb1d2f1d049
JKuziola <jakub.kuziola1@gmail.com>
2023-11-08 18:30:06 +0100

    ZPP-10 not working container

--- src/main/java/pl/mimuw/zpp/quantumai/utmpython/SolveService.java
+++ src/main/java/pl/mimuw/zpp/quantumai/utmpython/SolveService.java
@@ -7,6 +7,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.MountableFile;
 
 import static java.io.File.createTempFile;
 
@@ -14,22 +19,35 @@ import static java.io.File.createTempFile;
 @Slf4j
 public class SolveService {
 
-    public String solve(
-            File pythonFile,
-            File inputFile
-    ) throws IOException, InterruptedException {
-        String pythonFileAbsolutePath = pythonFile.getAbsolutePath();
-        File outputFile = createTempFile("output", "txt");
-        ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonFileAbsolutePath)
-                .redirectInput(inputFile)
-                .redirectOutput(outputFile);
-        Process process = processBuilder.start();
-        process.waitFor();
-        String result = Files.readString(Path.of(outputFile.getAbsolutePath()));
-        deleteFile(outputFile);
+    public String solve(File pythonFile, File inputFile) throws IOException, InterruptedException {
+        try (GenericContainer<?> container = new GenericContainer<>("python:latest")
+                .withCommand("sh", "-c", "python3 /mnt/user_code.py < /mnt/input.txt > /mnt/output.txt")
+                .withCopyFileToContainer(MountableFile.forHostPath(pythonFile.getAbsolutePath()), "/mnt/user_code.py")
+                .withCopyFileToContainer(MountableFile.forHostPath(inputFile.getAbsolutePath()), "/mnt/input.txt")
+                .waitingFor(Wait.forLogMessage(".*Container started.*", 1))
+                .withStartupTimeout(Duration.ofSeconds(30))) {
+            container.start();
 
-        return result;
+            // Your code to interact with the container
+            // (You can add any additional interactions with the container here if needed)
+
+            // Wait for the container to finish
+            container.followOutput(outputFrame -> {
+                // Handle the output frame if necessary
+            });
+
+            // After the container has run, copy the output file from the container to a local file
+            File outputFile = new File("path_to_local_output.txt");
+            // container.copyFileFromContainer("/mnt/output.txt", MountableFile.forHostPath(outputFile.getAbsolutePath()));
+
+            return Files.readString(outputFile.toPath());
+        } catch (Exception e) {
+            log.error("Error while running the Python script in a container: {}", e.getMessage());
+            throw e;
+        }
     }
+
+
 
     private static void deleteFile(File file) {
         if (!file.delete()) {
