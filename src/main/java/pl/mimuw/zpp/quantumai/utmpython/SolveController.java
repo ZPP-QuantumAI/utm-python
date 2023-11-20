f891bffc701fb50492aebe61df42886b6e66824b
wiktoor <74465264+wiktoor@users.noreply.github.com>
2023-11-20 16:39:46 +0100

    added option to read input as string not file (#6)
    * added option to read input as string not file
    
    * added workflow

--- src/main/java/pl/mimuw/zpp/quantumai/utmpython/SolveController.java
+++ src/main/java/pl/mimuw/zpp/quantumai/utmpython/SolveController.java
@@ -4,14 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Callable;
 
 import static java.io.File.createTempFile;
 
@@ -29,9 +30,24 @@ public class SolveController {
     public ResponseEntity<String> solve(
             @RequestPart MultipartFile pythonFile,
             @RequestPart MultipartFile inputFile
-    ) throws IOException, InterruptedException {
-        File python = tempFile(pythonFile);
-        File input = tempFile(inputFile);
+    ) throws Exception {
+        return solve(() -> tempFile(pythonFile), () -> tempFile(inputFile));
+    }
+
+    @PostMapping("/solve-string")
+    public ResponseEntity<String> solve(
+            @RequestParam String programText,
+            @RequestParam String inputText
+    ) throws Exception {
+        return solve(() -> tempFile(programText), () -> tempFile(inputText));
+    }
+
+    private ResponseEntity<String> solve(
+            Callable<File> pythonCallable,
+            Callable<File> inputCallable
+    ) throws Exception {
+        File python = pythonCallable.call();
+        File input = inputCallable.call();
 
         String result = solveService.solve(python, input);
 
@@ -45,6 +61,12 @@ public class SolveController {
         File file = createTempFile("temp", null);
         multipartFile.transferTo(file);
         return file;
+    }
+
+    private static File tempFile(String fileContent) throws IOException {
+        Path filePath = Files.createTempFile("temp", null);
+        Files.write(filePath, fileContent.getBytes(), StandardOpenOption.WRITE);
+        return filePath.toFile();
     }
 
     private static void deleteFile(File file) {
